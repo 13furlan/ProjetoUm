210b5e09446fb63655f23c0c242f7817c6fd8810
Lucas Furlan <lucas.ribeiro@dextra-sw.com>
2017-08-29 17:16:57 -0300

    A79 e A80 - Concatenando e minificando arquivos CSS e JS, melhorando tarefas do Gulp e criando a tarefa default

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,65 @@
-var gulp    = require('gulp'),
-    sass  = require('gulp-sass'),
-    include  = require('gulp-file-include'),
-    clean  = require('gulp-clean'),
+var gulp          = require('gulp'),
+    sass          = require('gulp-sass'),
+    include       = require('gulp-file-include'),
+    clean         = require('gulp-clean'),
     autoprefixer  = require('gulp-autoprefixer'),
-    uncss  = require('gulp-uncss'),
-    browserSync = require('browser-sync');
+    uncss         = require('gulp-uncss'),
+    imagemin      = require('gulp-imagemin'),
+    cssnano       = require('gulp-cssnano'),
+    concat        = require('gulp-concat'),
+    uglify        = require('gulp-uglify'),
+    browserSync   = require('browser-sync');
 
-gulp.task('clean', function() {
+gulp.task('clean', function(){
   return gulp.src('dist')
     .pipe(clean())
 })
 
 gulp.task('copy', ['clean'], function(){
-  gulp.src([
-      'src/components/**/*',
-      'src/css/**/*',
-      'src/js/**/*',
-      'src/img/**/*'],
-        {"base": "src"})
-    .pipe(gulp.dest('dist'))
+  gulp.src('src/components/**/*', {"base": "src"})
+      .pipe(gulp.dest('dist'))
 })
 
 gulp.task('sass', function(){
   gulp.src('./src/sass/**/*.scss')
     .pipe(sass())
     .pipe(autoprefixer())
+    .pipe(cssnano())
     .pipe(gulp.dest('./dist/css/'));
 })
 
 gulp.task('html', function(){
-  return gulp.src('./src/**/*.html')
-    .pipe(include())
-    .pipe(gulp.dest('./dist/'))
+  return gulp.src(['./src/**/*.html', '!src/inc/**/*'])
+            .pipe(include())
+            .pipe(gulp.dest('./dist/'))
 })
 
 gulp.task('uncss', ['html'], function(){
   return gulp.src('./dist/components/**/*.css')
-        .pipe(uncss({
-          html: ['./dist/*.html']
-        }))
-        .pipe(gulp.dest('./dist/components/'))
+            .pipe(uncss({
+              html: ['./dist/*.html']
+            }))
+            .pipe(gulp.dest('./dist/components/'))
 })
 
-gulp.task('server', ['uncss'], function(){
+gulp.task('imagemin', function(){
+  return gulp.src('./src/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/img/'))
+})
+
+gulp.task('build-js', function(){
+  gulp.src('./src/js/**/*')
+      .pipe(concat('app.min.js'))/* junta os arquivos em um só */
+      .pipe(uglify())/* minifica o arquivo juntado JS */
+      .pipe(gulp.dest('./dist/js/'))
+})
+
+gulp.task('default', ['copy'], function(){
+  gulp.start('uncss', 'imagemin', 'sass', 'build-js')
+})
+
+gulp.task('server', function(){
   browserSync.init({
     server: {
       baseDir: 'dist'
@@ -50,5 +67,6 @@ gulp.task('server', ['uncss'], function(){
   })
   gulp.watch('./dist/**/*').on('change', browserSync.reload)
   gulp.watch('./src/sass/**/*.scss', ['sass'])
+  gulp.watch('./src/js/**/*', ['build-js'])
   gulp.watch('./src/**/*.html', ['html'])
 })
